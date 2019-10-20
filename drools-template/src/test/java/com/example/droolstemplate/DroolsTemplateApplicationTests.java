b8cd89f65b00ba0079b2baf9e60fddc6a2e0b99f
slyle <lshem16@gmail.com>
2019-10-20 15:10:42 -0600

    springboot initializr

---
+++ drools-template/src/test/java/com/example/droolstemplate/DroolsTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.droolstemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
